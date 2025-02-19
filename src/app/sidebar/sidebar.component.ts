b936cd45bd20f83d594a13646b87fdcdeb39fc15
aki-masaki <nikistefan95@gmail.com>
2025-02-19 17:24:24 +0000

    added sidebar

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import {VisualGraph} from '../models/visual-graph.model';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.sass'
+})
+export class SidebarComponent {
+}
